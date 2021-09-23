f66a97f94cb9f75d1b40b38201158db0931da0b1
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-09-23 17:03:22 +0100

    :art: config app structure
    setup ESLint, Prettier, folder structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import PropTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.shape(PropTypes.any).isRequired,
+};
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
     extend: {},
   },
@@ -9,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
